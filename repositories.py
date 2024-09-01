9907a76044387ba279817205806525ad3a50af8f
anderslatif <anderslatif@users.noreply.github.com>
2024-09-01 10:07:05 +0200

    Removed the example group from repositories.py

--- repositories.py
+++ repositories.py
@@ -1,15 +1,5 @@
 GROUP_REPOS = [
         {
-            "name": "Example Group",
-            "gitLinks": [""],
-            "backend": "http(s)://<IP/DOMAIN>/<APIURL>",
-            "frontend": "http(s)://<IP/DOMAIN>/<FrontEndURL>",
-            "monitoring": "http(s)://<IP/DOMAIN>/<MonitoringURL>",
-            "stack": ["Flask", "Svelte", "CouchDB", "Redis"],
-            "documentation": ["link to documentation", "another link if it applies", "et cetera"],
-            "sla": "link to sla",
-        },
-        {
             "name": "Team AB",
             "gitLinks": ["https://github.com/Team-AB-DevOps/ab-dev-ops"],
             "backend": "",
