cbe81d944ea4fa09ce6518f834c7c5313aeed5dc
Jack Hansen <jackvwh@hotmail.com>
2024-11-17 09:57:48 +0100

    Update Git link for MadeInChina-DevOps repository in repositories.py

--- repositories.py
+++ repositories.py
@@ -51,7 +51,7 @@ GROUP_REPOS = [
     },
     {
         "name": "MadeInChina-DevOps",
-        "gitLinks": ["https://github.com/MadeInChina-DevOps/whoknows_nooneknows"],
+        "gitLinks": ["https://github.com/MadeInChina-DevOps/whoknowssearch"],
         "backend": "https://whoknowssearch.com",
         "frontend": "https://whoknowssearch.com",
         "monitoring": "",
